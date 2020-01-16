88173b69e60a1eaf5353ab8d3963933cd3950fca
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-01-16 18:07:54 -0500

    login page base structure completed

--- client/src/App/components/formComponent/formComponent.js
+++ client/src/App/components/formComponent/formComponent.js
@@ -1,10 +1,121 @@
+// import React, { Fragment } from 'react';
+// import { FormControl, Button, TextField, Typography, Grid } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+// const useStyles = makeStyles(theme =>({
+//     gridContainer: {
+        
+//     },
+//     form: {
+//         width: "100%",
+//         marginTop: theme.spacing(3)
+//     }
+// }));
+// function FormComponent(props)
+// {
+//     const cssStyle = useStyles();
+//     const handleSubmit = (event) =>{
+//         event.preventDefault();
+//         console.log(`form submitted`);
+//     }
+
+//     const handleChange = (event) =>{
+//         console.log(`stuff changed, this is what changed it:`);
+//         console.log(event.target);
+//     }
+    
+//     return (
+//         <div>
+//             <Typography component="h1" variant="h5" align="center">
+//                 Sign In
+//             </Typography>
+//             <form onSubmit={handleSubmit} className={cssStyle.form}>
+//                 <FormControl>
+//                     <TextField
+//                     id="userName"
+//                     label="username"
+//                     variant="outlined"
+//                     color="primary"
+//                     //value={// TODO */}
+//                     onChange={handleChange}
+//                     >
+//                     </TextField>
+
+//                     <TextField
+//                     id="password"
+//                     label="password"
+//                     variant="outlined"
+//                     color="primary"
+//                     type="password"
+//                     >
+//                     </TextField>
+
+//                 </FormControl>
+//                 <FormControl>
+//                     <Button type="submit" >Submit</Button>
+//                 </FormControl>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default FormComponent;
+
 import React from 'react';
+import {Grid, Typography, TextField, Button} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles(theme =>({
+    
+}));
 
 function FormComponent(props)
 {
-    return (<p>
-        Im form component
-    </p>)
+
+    
+    const handleChange = (event) =>{
+        console.log(`stuff changed, this is what changed it:`);
+        console.log(event.target);
+    }
+
+    const handleSubmit= (event)=>{
+        event.preventDefault();
+        console.log(`form submitted`);
+        console.log(event);
+    }
+    return (
+        <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
+            
+            <Grid item md={12} sm={12} xs={12}>
+                <Typography component="h1" variant="h5">Sign In</Typography>
+            </Grid>
+            <Grid item md={12} sm={12} xs={12}>
+                <TextField
+                    label="username"
+                    name="username"
+                    type="text"
+                    variant="outlined"
+                    color="primary"
+                    //value={// TODO */}
+                    onChange={handleChange}
+                    >
+                    </TextField>
+            </Grid>
+            <Grid item>
+            <TextField
+                    name="password"
+                    label="password"
+                    variant="outlined"
+                    color="primary"
+                    type="password"
+                    //value={// TODO */}
+                    onChange={handleChange}
+                    >
+                    </TextField>
+            </Grid>
+            <Grid item>
+                <Button onClick={handleSubmit}>Submit</Button>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default FormComponent;
--- client/src/App/pages/login.js
+++ client/src/App/pages/login.js
@@ -1,32 +1,69 @@
-import React, { Fragment } from 'react';
-import { Container, Paper, Card, CardContent, CardMedia } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import FormComponent from '../components/formComponent/formComponent';
+import React, { Fragment } from 'react'
+import {makeStyles } from '@material-ui/core/styles';
+import { CssBaseline, Grid, Paper } from '@material-ui/core';
+import FormComponent from '../components/formComponent/formComponent'
 const useStyles = makeStyles(theme =>({
-    container: {
-        minHeight: "100vh",
-        minWidth: "100vh",
-        backgroundColor: theme.palette.primary[50],
+    root : {
+        height: "100vh",
+        background: "#6190E8",
+        background: "linear-gradient(220deg, #A7BFE8, #5a8ae6)"
+        
+    },
+    image: {
+        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: "100%"
+        },
+
+    gridItem: {
+        height: "100%",
+    },
+    paperContainer: {
+        height: "80%",
+        marginTop: "6%"
     }
-       
-}));
-
-
+}))
 
 function Login(props)
 {
     const cssStyle = useStyles();
     return (
         <Fragment>
-            <Container className={cssStyle.container} disableGutters={true}>
-                <Card>
-                    <CardContent>
-                        <FormComponent></FormComponent> {/* Widget 1 */}
-                    </CardContent>
-                </Card>
-            </Container>
+            <CssBaseline />
+            <Grid container  component="main" className={cssStyle.root} justify="center">
+                <Grid item md={8} sm={12} xs={12} className={cssStyle.gridItem}>
+
+                    <Grid  container  direction="row"  justify="center"  alignItems="stretch"  className={cssStyle.paperContainer} component={Paper} elevation={12} >
+
+                        <Grid item md={6} sm={6} xs={false} className={cssStyle.image} ></Grid>
+                        <Grid item md={6} sm={6} xs={12}>
+                            <FormComponent/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Fragment>
     )
 }
-
 export default Login;
+// function Login(props){
+//     const cssStyle = useStyles();
+//     return (
+//         <Grid Container className={cssStyle.root} >
+//             {/* <CssBaseline /> */}
+//             <Grid item md={7}>
+//                 <Grid Container spacing={10}>
+//                 <Grid item xs={false} sm={4} md={7} className={cssStyle.image} >test</Grid>
+//                     <Grid item md={6}>
+//                         <p>hello111</p>
+//                     </Grid>
+//                 </Grid>
+//             </Grid>
+
+//         </Grid>
+//     );
+// }
+
+// export default Login;
