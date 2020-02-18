31c154e02d03424ecaa16207a1c48146150c28af
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-02-18 11:15:25 -0500

    registration form validation added

--- client/src/App/components/shared/registerUser/registerUser.js
+++ client/src/App/components/shared/registerUser/registerUser.js
@@ -7,6 +7,79 @@ function RegisterUser(props) {
     const theme = useTheme();
 
     const [userInfo, setUserInfo] = useState(INITIAL_STATE);
+    const [formState, setFormState] = useState({firstName:{
+        isValid:true,
+        errorMessage: "",},
+        lastName: {
+            isValid:true,
+            errorMessage:"",
+        },
+        email: {
+            isValid:true,
+            errorMessage:"",
+        },
+        userName:{
+            isValid: true,
+            errorMessage: ""
+        },
+        password:{
+            isValid: true,
+            errorMessage: ""
+        },
+        confirmPassword:{
+            isValid: true,
+            errorMessage: ""
+        }
+
+    });
+
+    const validateUsername = (event)=>{
+        let pattern = new RegExp(/^[a-z0-9_-]{3,15}$/);
+        let result = pattern.test(event.target.value);
+        if(result == false && event.target.value != "")
+        {
+            
+            handleFormState({[event.target.name]:{isValid: false, errorMessage:`only alphanumeric, '_' and '-' allowed`}});
+        }
+        else{
+            handleFormState({[event.target.name]:{isValid: true, errorMessage:``}});
+        }
+    }
+
+    const validatePassword = ()=>{
+
+        if(userInfo.password == userInfo.confirmPassword)
+        {
+            handleFormState({["password"]:{isValid: true, errorMessage:``}});
+        }
+        else{
+            handleFormState({["password"]:{isValid: false, errorMessage:`password does not match confirm password`}});
+        }
+    }
+
+    const validateEmail = (event) =>{
+        let pattern = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
+        let result = pattern.test(event.target.value);
+        if(result == false && event.target.value != "")
+        {
+            handleFormState({[event.target.name]:{isValid: false, errorMessage:`email validation failed. make sure it follows format example@domain.com`}});
+        }
+        else{
+            handleFormState({[event.target.name]:{isValid: true, errorMessage:``}});
+        }
+    }
+    
+    const validateName = (event)=>{
+        let pattern = new RegExp(/^[a-zA-Z ]*$/);
+        let result = pattern.test(event.target.value);
+        if(result == false && event.target.value != "")
+        {
+            handleFormState({[event.target.name]:{isValid: false, errorMessage:`only letters allowed for ${event.target.name}`}});
+        }
+        else{
+            handleFormState({[event.target.name]:{isValid: true, errorMessage:``}});
+        }
+    }
 
     const handleChange = (event) => {
         const {name, value} = event.target;
@@ -18,13 +91,17 @@ function RegisterUser(props) {
 
     }
 
+    const handleFormState = (newState)=>{
+
+        setFormState((currentState)=>({
+            ...currentState,
+            ...newState
+        }));
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(`form submitted`);
-        console.log(userInfo)
 
-        //TODO: Call validate function
-        //TODO: Loading spinner implimentation?
         fetch( (process.env.REACT_APP_PUBLIC_URL? process.env.REACT_APP_PUBLIC_URL : "") + "api/register",{
             method: "POST",
             headers: {
@@ -64,6 +141,9 @@ function RegisterUser(props) {
                     type="text"
                     variant="standard"
                     color="primary"
+                    error={!formState.firstName.isValid}
+                    onBlur={validateName}
+                    helperText={formState.firstName.errorMessage}
                     fullWidth
                     onChange={handleChange}
 
@@ -75,6 +155,9 @@ function RegisterUser(props) {
                     label="your last name"
                     name="lastName"
                     type="text"
+                    error={!formState.lastName.isValid}
+                    onBlur={validateName}
+                    helperText={formState.lastName.errorMessage}
                     variant="standard"
                     color="primary"
                     fullWidth
@@ -83,7 +166,9 @@ function RegisterUser(props) {
             <Grid item md={12} sm={12} xs={12}>
                 <TextField
                     onChange={handleChange}
-
+                    onBlur={validateEmail}
+                    helperText={formState.email.errorMessage}
+                    error={!formState.email.isValid}
                     label="your email"
                     name="email"
                     type="text"
@@ -97,7 +182,9 @@ function RegisterUser(props) {
             <Grid item md={12} sm={12} xs={12}>
                 <TextField
                     onChange={handleChange}
-
+                    onBlur={validateUsername}
+                    helperText={formState.userName.errorMessage}
+                    error={!formState.userName.isValid}
                     label="your username"
                     name="userName"
                     type="text"
@@ -110,7 +197,9 @@ function RegisterUser(props) {
             <Grid item md={6} sm={5} xs={12}>
                 <TextField
                     onChange={handleChange}
-
+                    onBlur={validatePassword}
+                    helperText={formState.password.errorMessage}
+                    error={!formState.password.isValid}
                     label="password"
                     name="password"
                     type="password"
@@ -122,7 +211,7 @@ function RegisterUser(props) {
             <Grid item md={6} sm={5} xs={12}>
                 <TextField
                     onChange={handleChange}
-
+                    onBlur={validatePassword}
                     label="confirm password"
                     name="confirmPassword"
                     type="password"
