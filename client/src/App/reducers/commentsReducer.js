aa5eac70a62d432a63c89b145174bc30a7f0d438
Abdul Rehman <abdul-rehman@myseneca.ca>
2020-02-24 09:51:50 -0500

    recursive search algorithem draft

--- client/src/App/reducers/commentsReducer.js
+++ client/src/App/reducers/commentsReducer.js
@@ -7,11 +7,24 @@ function commentsReducer(currentState ={data: []}, actionRequested) {
           data: [...actionRequested.payload]
         };
       case "ADD_COMMENT":
-        console.log(actionRequested.payload);
-        //todo: Find and append to a nested object using id
-        
-        return currentState;
-      case "DEFAULT_STATE":
+
+      
+        let res = findObjectById(currentState.data,actionRequested.payload.parent);
+        const newItem = {
+          comment: actionRequested.payload.comment,
+          author: actionRequested.payload.author,
+          edited: false,
+          comments: []
+        };
+        //TODO: Use Immer to manually mutate the current state and append node. in future flatten your state
+
+        return {
+          ...currentState,
+          data: [...currentState, ...res.comments.concat(newItem)]
+          //TODO: This does not work for nested nodes
+          // data: currentState.data.map(d => d._id === actionRequested.payload.parent ? ({ ...d, comments: d.comments.concat(newItem) }) : d) 
+        }        
+        case "DEFAULT_STATE":
         return {
           data: []
         }
@@ -19,5 +32,39 @@ function commentsReducer(currentState ={data: []}, actionRequested) {
         return currentState;
     }
   }
+
+  function findObjectById(root, id) {
+    console.log(`func call with`);
+    console.log(root);
+
+    for(let i =0; i < root.length; i++)
+    {
+      if(root[i]._id == id)
+      {
+        console.log(`found a match`);
+        console.log(root[i]);
+        return root[i];
+      }
+      if(root[i].comments.length > 0)
+      {
+        console.log(`going to next phase with root....`);
+        console.log(root[i].comments);
+        console.log(root[i].comments.length > 0)
+        console.log(root[i]._id);
+
+        console.log(`calling recrsuve function with`);
+        console.log(root[i].comments)
+        let ans = findObjectById(root[i].comments,id);
+        console.log(`node retured from stack is...`);
+        console.log(ans);
+        if(ans != null)
+        {
+          return ans;
+        }
+      }
+    }
+    return null;
+};
+
   
 export default commentsReducer;
